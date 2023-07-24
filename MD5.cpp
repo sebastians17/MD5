ee64b01a80bf4463d84e4c71d96f48c8a933720c
sebastians17 <sebastians17@onet.pl>
2023-07-24 16:45:54 +0200

    Added string variable with MD5

--- MD5.cpp
+++ MD5.cpp
@@ -1,4 +1,6 @@
+#include <iostream>
 #include <stdio.h>
+#include <string>
 #include <openssl/evp.h>
 
 int main() {
@@ -6,6 +8,7 @@ int main() {
     FILE* inFile = fopen(filename, "rb");
     int bytes;
     unsigned char data[1024];
+    std::string checksum;
 
     EVP_MD_CTX* mdctx;
     unsigned int md5_digest_len = EVP_MD_size(EVP_md5());
@@ -25,9 +28,18 @@ int main() {
     unsigned char* md5_digest = static_cast<unsigned char*>(OPENSSL_malloc(md5_digest_len));
     EVP_DigestFinal_ex(mdctx, md5_digest, &md5_digest_len);
     EVP_MD_CTX_free(mdctx);
+
+    // Print MD5
     for (int i = 0; i < md5_digest_len; i++) {
         printf("%02x", md5_digest[i]);
     }
+
+    // MD5 to string
+    checksum.resize(md5_digest_len * 2);
+    for (unsigned int i = 0; i < md5_digest_len; ++i) {
+        std::sprintf(&checksum[i * 2], "%02x", md5_digest[i]);
+    }
+    std::cout << "MD5: " << checksum << std::endl;
 
     return 0;
 }
